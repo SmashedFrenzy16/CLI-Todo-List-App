e6993ca8c14064fd653350eea8e1e07a50e2a19d
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-03-03 20:45:27 +0000

    Update todoapp.py

--- Todo-App/todoapp.py
+++ Todo-App/todoapp.py
@@ -3,4 +3,11 @@ import datetime
 
 def help():
 
-    pass
+    sa = """Usage :-
+$ ./todo add <todo item>  # Add a new todo item
+$ ./todo ls               # Show remaining todo items
+$ ./todo del NUMBER       # Delete a todo item
+$ ./todo done NUMBER      # Complete a todo item
+$ ./todo help             # Help menu
+$ ./todo report           # Statistics"""
+    sys.stdout.buffer.write(sa.encode('utf8'))
